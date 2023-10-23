17e5ab22e8ad5affeba7aba311e6d9d4185ec605
Rukiye Balı <145977861+RukiyeBali@users.noreply.github.com>
2023-10-23 14:52:52 +0300

    Add files via upload

---
+++ for-örnek/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_örnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int  sayi1, sayi2;
+            int toplam = 0;
+            sayi1=Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+         
+            for (int i = sayi1; i <= sayi2; i++)
+            {
+                toplam += i;
+            }
+            label1.Text=toplam.ToString();
+
+        }
+    }
+}
